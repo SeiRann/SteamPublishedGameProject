0bf68faa68666a8da96d1f38e0e7920ef4263f32
SeiRan <bo646ru@gmail.com>
2026-03-13 16:16:55 -0400

    Toggle Gui with camera and cursor lock

--- Assets/InputSystem/PlayerAbilityScript.cs
+++ Assets/InputSystem/PlayerAbilityScript.cs
@@ -1,8 +1,15 @@
 using System.Collections.Generic;
+using System.Collections;
 
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Net;
+#endif
 
 public class PlayerAbilityScript : MonoBehaviour
 {
@@ -10,12 +17,14 @@ public class PlayerAbilityScript : MonoBehaviour
     public bool canDraw = true;
     public static List<Renderer> scannableObjects = new List<Renderer>();
     UIDocument DrawGui;
+    FirstPersonController playerController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         DrawGui = GetComponent<UIDocument>();
-        
+        Debug.Log(DrawGui);
+        playerController = GetComponent<FirstPersonController>();
     }
 
     public void Scan(InputAction.CallbackContext context) {
@@ -30,9 +39,23 @@ public class PlayerAbilityScript : MonoBehaviour
         }
     }
 
-    public void toggleDrawing() {
-        if (canDraw) {
+    public void toggleDrawing(InputAction.CallbackContext context) {
+
+        if (canDraw && context.performed) {
+            Debug.Log("toggled drawing");   
+
             DrawGui.enabled = !DrawGui.enabled;
+
+            if(DrawGui.enabled == true)
+            {
+                UnityEngine.Cursor.lockState = CursorLockMode.None;
+                playerController.cameraCanMove = false;
+            }
+            else
+            {
+                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                playerController.cameraCanMove = true;
+            }
         }
     }
 
@@ -41,7 +64,7 @@ public class PlayerAbilityScript : MonoBehaviour
         if(other.tag == "Scannable")
         {
             scannableObjects.Add(other.GetComponent<Renderer>());
-            Debug.Log(scannableObjects.Count);
+            //Debug.Log(scannableObjects.Count);
         }
     }
 
@@ -50,7 +73,7 @@ public class PlayerAbilityScript : MonoBehaviour
         if(other.tag == "Scannable")
         {
             scannableObjects.Remove(other.GetComponent<Renderer>());
-            Debug.Log(scannableObjects.Count);
+            //Debug.Log(scannableObjects.Count);
         }
     }
 
